f0398e36ce6c256672fcc0c93295ec5e10a4cd72
Yahya Haffejee <yahya.saarchtech6@gmail.com>
2024-10-29 11:16:56 +0000

    complete

--- incident-management/tests/test.js
+++ incident-management/tests/test.js
@@ -2,7 +2,7 @@ const cds = require('@sap/cds/lib')
 const { default: axios } = require('axios')
 const { GET, POST, DELETE, PATCH, expect } = cds.test(__dirname + '../../')
 
-axios.defaults.auth = { username: '${CDS_USER_YAHYA_EMAIL}', password: '${CDS_USER_YAHYA_PASSWORD}' }
+axios.defaults.auth = { username: 'incident.support@tester.sap.com', password: 'initial' }
 
 jest.setTimeout(11111)
 
